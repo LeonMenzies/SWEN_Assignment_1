f89622d0607b9f1a9b2a7936b29ee9f8dc17f0d3
Harry Taylor <harry.n.taylor@gmail.com>
2021-07-18 17:23:45 +1200

    updated player class

--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,22 @@
 import java.util.*;
 
 
+
 public class Player extends Move {
-    private boolean turn;
+    private boolean turn = false;
+    private String name;
+    private int steps = 0;
 
     private List<Card> hand;
-    private List<Estate> estates;
-    private List<Die> dies;
+   // private List<Estate> estates;
+  //  private List<Die> dies;
 
-    public Player(boolean aTurn) {
-        super();
-        turn = aTurn;
+    public Player(String name) {
+     //   super();
+        this.name = name;
         hand = new ArrayList<>();
-        estates = new ArrayList<>();
-        dies = new ArrayList<>();
+       // estates = new ArrayList<>();
+       // dies = new ArrayList<>();
     }
 
     public void setTurn(boolean aTurn) {
@@ -28,13 +31,13 @@ public class Player extends Move {
         return this.hand;
     }
 
-    public List<Estate> getEstates() {
-        return this.estates;
-    }
+//    public List<Estate> getEstates() {
+//        return this.estates;
+//    }
 
-    public List<Die> getDies() {
-        return this.dies;
-    }
+//    public List<Die> getDies() {
+//        return this.dies;
+//    }
 
     public void addHand(Card card) {
         this.hand.add(card);
@@ -44,21 +47,21 @@ public class Player extends Move {
         this.hand.remove(card);
     }
 
-    public void addEstate(Estate aEstate) {
-        this.estates.add(aEstate);
-    }
-
-    public void removeEstate(Estate aEstate) {
-        this.estates.remove(aEstate);
-    }
-
-    public void addDy(Die aDy) {
-        this.dies.add(aDy);
-    }
-
-    public void removeDy(Die aDy) {
-        this.dies.remove(aDy);
-    }
+//    public void addEstate(Estate aEstate) {
+//        this.estates.add(aEstate);
+//    }
+//
+//    public void removeEstate(Estate aEstate) {
+//        this.estates.remove(aEstate);
+//    }
+//
+//    public void addDy(Die aDy) {
+//        this.dies.add(aDy);
+//    }
+//
+//    public void removeDy(Die aDy) {
+//        this.dies.remove(aDy);
+//    }
 
     public String toString() {
         return super.toString() + "[" + "turn" + ":" + getTurn() + "]";
